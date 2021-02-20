1472b9dda5f3e1f7b54cdcc64b905bb6d736651f
Samuel Scherer <swiimii962@gmail.com>
2021-02-20 15:32:56 -0500

    Stop trying to control network players

---
+++ Assets/Scripts/Player/NetworkPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class NetworkPlayer : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            if (GetComponentInChildren<AudioListener>())
+            {
+                GetComponentInChildren<AudioListener>().enabled = false;
+            }
+            if (GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>())
+            {
+                GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = false;
+            }
+        }
+    }
+
+}
