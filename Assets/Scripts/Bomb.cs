4836160a9b43cb06970ea6dcc233f926c844713c
Samuel Scherer <swiimii962@gmail.com>
2021-02-21 07:22:26 -0500

    Bomb opens oxygen room

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Bomb : Moveable
+{
+    public void Detonate()
+    {
+        NetworkManager.Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/BombTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class BombTrigger : NetworkBehaviour
+{
+    [SerializeField] Door door;
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(isServer)
+        {
+            if (other.GetComponent<Bomb>())
+            {
+                other.GetComponent<Bomb>().Detonate();
+                door.StartCoroutine("OpenDoor");
+            }
+        }
+    }
+}
