652774679255fc3829ae5b557bf584c8db1b0a58
Samuel Scherer <swiimii962@gmail.com>
2021-02-21 03:15:06 -0500

    Lights can be turned on

---
+++ Assets/Scripts/LightMode.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class LightMode : NetworkBehaviour
+{
+    public GameObject[] dimLights; 
+    public GameObject[] brightLights;
+    public bool status = false;
+
+    public void DimLights()
+    {
+        status = false;
+        RpcDimLights();
+        
+    }
+    public void BrightLights()
+    {
+        status = true;
+        RpcBrightLights();
+    }
+    private void RpcDimLights()
+    {
+        foreach (GameObject light in dimLights)
+        {
+            light.SetActive(true);
+        }
+        foreach (GameObject light in brightLights)
+        {
+            light.SetActive(false);
+        }
+    }
+    private void RpcBrightLights()
+    {
+        foreach (GameObject light in dimLights)
+        {
+            light.SetActive(false);
+        }
+        foreach (GameObject light in brightLights)
+        {
+            light.SetActive(true);
+        }
+    }
+    
+}
---
+++ Assets/Scripts/LightsLever.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsLever : Interactable
+{
+    [SerializeField] LightMode lights;
+    public override void Interact()
+    {
+        lights.BrightLights();
+        base.Interact();
+    }
+}
