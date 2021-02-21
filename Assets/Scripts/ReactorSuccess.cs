9eac01604e24c8910e42cd6a9d5b32e6054fde2e
Samuel Scherer <37094203+swiimii@users.noreply.github.com>
2021-02-21 12:45:20 -0500

    Add win condition! Almost done! (#9)
    * Add sounds to doors, keypad
    
    * Untracked scene changes from last commit
    
    * Add starting music
    
    * Reduce timer to 6 minutes
    
    * Add clank noise when door fails to open
    
    * Add victory condition!

---
+++ Assets/Scripts/ReactorSuccess.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class ReactorSuccess : ChainEvent
+{
+    public GameObject lights;
+    public GameState gameState;
+    public override void DoEvent()
+    {
+        lights.SetActive(true);
+        GetComponent<AudioSource>().Play();
+        gameState.reactorSuccess = true;
+        gameState.CheckWin();
+    }
+}
---
+++ Assets/Scripts/VialParent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class VialParent : NetworkBehaviour
+{   
+    // Index in this array corresponds with VialColor enum
+    public ChainEvent chainEvent;
+    public bool[] vialStatuses;
+    private bool isDone = false;
+    public void Start()
+    {
+        vialStatuses = new bool[4];
+        for (int i = 0; i < 4; i++)
+        {
+            vialStatuses[i] = false;
+        }
+    }
+
+    public void CheckStatus()
+    {
+        if (!isDone)
+        {
+            foreach (bool b in vialStatuses)
+            {
+                if (!b)
+                {
+                    return;
+                }
+            }
+            isDone = true;
+            chainEvent.DoEvent();
+        }
+    }
+
+}
